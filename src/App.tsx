557fb37b82f4559733473447d43f413188b0081a
Zsolt Kovacs <kzsolt82@gmail.com>
2024-07-04 16:03:48 +0300

    add - gitignore file

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home.tsx'
+import About from './Pages/About.tsx'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<h1>404 Page not found</h1>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
